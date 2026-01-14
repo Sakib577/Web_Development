c3de770331bb59680e40428d44a4c2518e3a0141
Abdullah As-Sakib <sakib5800.sk.3@gmail.com>
2026-01-15 01:05:20 +0600

    Refactor code structure and remove redundant changes

---
+++ Javascript/Test.js
@@ -0,0 +1,8 @@
+
+            document.addEventListener('DOMContentLoaded', function(){
+            document.querySelector('form').addEventListener('submit', function (event){
+                
+                alert('hello, '+ document.querySelector('#name').value);
+                event.preventDefault();
+            });
+            });
